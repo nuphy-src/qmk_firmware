94a484d4109f487d2938c32c55f34e60222c78e0
Peter Fern <github@0xc0dedbad.com>
2024-06-16 06:50:03 +1000

    Move battery color to table and simplify light calculation

--- keyboards/nuphy/air75_v2/ansi/side.c
+++ keyboards/nuphy/air75_v2/ansi/side.c
@@ -511,32 +511,17 @@ void rf_led_show(void) {
 void bat_num_led(uint8_t bat_percent)
 {
     uint8_t r, g, b;
+    uint8_t bat_idx = bat_percent / 17;
+    uint8_t bat_lvl = 1 + ((bat_percent - 1) / 10);
 
-    // set color
-    if (bat_percent <= 15) {
-        r = 0xff; g = 0x00; b = 0x00;
-    }
-    else if (bat_percent <= 50) {
-        r = 0xff; g = 0x40; b = 0x00;
-    }
-    else if (bat_percent <= 80) {
-        r = 0xff; g = 0xff; b = 0x00;
-    }
-    else {
-        r = 0x00; g = 0xff; b = 0x00;
-    }
+    r = bat_percent_tab[bat_idx][0];
+    g = bat_percent_tab[bat_idx][1];
+    b = bat_percent_tab[bat_idx][2];
 
     // set percent
-    if (bat_percent >= 1) rgb_matrix_set_color(29, r, g, b);
-    if (bat_percent > 10) rgb_matrix_set_color(28, r, g, b);
-    if (bat_percent > 20) rgb_matrix_set_color(27, r, g, b);
-    if (bat_percent > 30) rgb_matrix_set_color(26, r, g, b);
-    if (bat_percent > 40) rgb_matrix_set_color(25, r, g, b);
-    if (bat_percent > 50) rgb_matrix_set_color(24, r, g, b);
-    if (bat_percent > 60) rgb_matrix_set_color(23, r, g, b);
-    if (bat_percent > 70) rgb_matrix_set_color(22, r, g, b);
-    if (bat_percent > 80) rgb_matrix_set_color(21, r, g, b);
-    if (bat_percent > 90) rgb_matrix_set_color(20, r, g, b);
+    for (uint8_t i = 0; i < bat_lvl ; i++) {
+        rgb_matrix_set_color(29 - i, r, g, b);
+    }
 }
 
 void num_led_show(void)
@@ -558,35 +543,15 @@ void bat_led_close(void)
  * @brief  bat_percent_led.
  */
 void bat_percent_led(uint8_t bat_percent) {
-    uint8_t bat_end_led = 0;
-    uint8_t bat_r, bat_g, bat_b;
+    uint8_t bat_idx = bat_percent / 17;
 
-    if (bat_percent <= 15) {
-        bat_end_led = 0;
-        bat_r = 0x80, bat_g = 0, bat_b = 0;
-    } else if (bat_percent <= 20) {
-        bat_end_led = 1;
-        bat_r = 0x80, bat_g = 0x40, bat_b = 0;
-    } else if (bat_percent <= 40) {
-        bat_end_led = 2;
-        bat_r = 0x80, bat_g = 0x40, bat_b = 0;
-    } else if (bat_percent <= 60) {
-        bat_end_led = 3;
-        bat_r = 0x80, bat_g = 0x40, bat_b = 0;
-    } else if (bat_percent <= 80) {
-        bat_end_led = 4;
-        bat_r = 0x80, bat_g = 0x40, bat_b = 0;
-    } else if (bat_percent <= 95) {
-        bat_end_led = 5;
-        bat_r = 0x80, bat_g = 0x40, bat_b = 0;
-    } else {
-        bat_end_led = 5;
-        bat_r = 0, bat_g = 0x80, bat_b = 0;
-    }
+    r_temp = bat_percent_tab[bat_idx][0];
+    g_temp = bat_percent_tab[bat_idx][1];
+    b_temp = bat_percent_tab[bat_idx][2];
 
     uint8_t i = 0;
-    for (; i <= bat_end_led; i++)
-        side_rgb_set_color(11 - i, bat_r >> 2, bat_g >> 2, bat_b >> 2);
+    for (; i <= bat_idx; i++)
+        side_rgb_set_color(11 - i, r_temp >> 2, g_temp >> 2, b_temp >> 2);
 
     for (; i < 6; i++)
         side_rgb_set_color(11 - i, 0, 0, 0);
