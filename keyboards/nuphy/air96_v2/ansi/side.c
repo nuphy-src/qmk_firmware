7abd595398dac2d0885809680f33a401e6825cf6
zhouzengming <1548602509@qq.com>
2025-03-16 17:52:34 +0800

    Add reverse NumLock indicator; Decrease brightness of CapsLock and NumLock; Change battery indicator: yellow when 50-95%.

--- keyboards/nuphy/air96_v2/ansi/side.c
+++ keyboards/nuphy/air96_v2/ansi/side.c
@@ -299,7 +299,7 @@ void sys_led_show(void)
 {
     if (dev_info.link_mode == LINK_USB) {
         if (host_keyboard_led_state().caps_lock) {
-            set_left_rgb(0X00, SIDE_BLINK_LIGHT, SIDE_BLINK_LIGHT);
+            set_left_rgb(0X00, SIDE_BLINK_LIGHT / 2, SIDE_BLINK_LIGHT / 2);
         }
         if (!host_keyboard_led_state().num_lock) {
             set_right_rgb(0X00, SIDE_BLINK_LIGHT / 2, SIDE_BLINK_LIGHT / 2);
