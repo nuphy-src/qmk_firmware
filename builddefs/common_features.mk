df799676e8df11812befe53b438e402568ebfbb7
zhouzengming <1548602509@qq.com>
2025-03-16 18:51:11 +0800

    Update including method.

--- builddefs/common_features.mk
+++ builddefs/common_features.mk
@@ -28,7 +28,6 @@ QUANTUM_SRC += \
     $(QUANTUM_DIR)/sync_timer.c \
     $(QUANTUM_DIR)/logging/debug.c \
     $(QUANTUM_DIR)/logging/sendchar.c \
-    $(QUANTUM_DIR)/signalrgb.c \
 
 VPATH += $(QUANTUM_DIR)/logging
 # Fall back to lib/printf if there is no platform provided print
@@ -632,12 +631,18 @@ ifeq ($(strip $(LED_TABLES)), yes)
     SRC += $(QUANTUM_DIR)/led_tables.c
 endif
 
-#ifeq ($(strip $(VIA_ENABLE)), yes)
+ifeq ($(strip $(VIA_ENABLE)), yes)
     DYNAMIC_KEYMAP_ENABLE := yes
     RAW_ENABLE := yes
     BOOTMAGIC_ENABLE := yes
     TRI_LAYER_ENABLE := yes
-#endif
+endif
+
+ifeq ($(strip $(SIGNALRGB_ENABLE)), yes)
+    RAW_ENABLE := yes
+    SRC += $(QUANTUM_DIR)/signalrgb.c
+    OPT_DEFS += -DSIGNALRGB_ENABLE
+endif
 
 VALID_CUSTOM_MATRIX_TYPES:= yes lite no
 
